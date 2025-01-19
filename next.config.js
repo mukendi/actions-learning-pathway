662250d66e0d9abbc1e0d6711cf5e8023f353250
mukendi <ngoymukendix@gmail.com>
2025-01-19 21:09:36 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,1 @@
-/** @type {import('next').NextConfig} */
-/**
-const nextConfig = {
-  output: 'export',
 
-  images: {
-    unoptimized: true,
-  },
-  
-  reactStrictMode: true,
-  extends: [
-    'plugin:@next/next/recommended',
-  ]
-}
-
-module.exports = nextConfig
-*/
